6a79de0165ea22a5b545ac125033b841a5a2d6b7
alakka <alakka@DIN66007316.corp.capgemini.com>
2018-07-16 10:49:46 +0530

    collections

--- src/main/java/com/capgemini/walletapp/service/WalletService.java
+++ src/main/java/com/capgemini/walletapp/service/WalletService.java
@@ -7,7 +7,7 @@ import com.capgemini.walletapp.dao.WalletDAO;
 
 public class WalletService implements IWalletService {
 
-	WalletDAO dao= new WalletDAO();
+	WalletDAO dao = new WalletDAO();
 
 	public boolean createAccount(WalletDetails details) {
 		// TODO Auto-generated method stub
@@ -15,39 +15,29 @@ public class WalletService implements IWalletService {
 	}
 
 	public int withdraw(double balance) {
-		return	dao.withdraw(balance);
-		 
-	}
+		return dao.withdraw(balance);
 
-	
+	}
 
 	public int fundTransfer(long accNum, double balance) {
-	
+
 		return dao.fundTransfer(accNum, balance);
 	}
-public boolean login(String uname,String pass) {
-	
-	return  dao.login(uname,pass);
-}
+
+	public boolean login(String uname, String pass) {
+		return dao.login(uname, pass);
+	}
+
 	public List printTransaction() {
 		return dao.printTrans();
 	}
 
 	public double showBal() {
-		
 		return dao.showBal();
 	}
 
-	
-
-
 	public int deposit(double balance) {
-		
 		return dao.deposit(balance);
 	}
-
-	
-	
-	
 
 }
